c0cea4585af241a8c7cd038a37804e0663a90aa2
Alishan_Xd <154049785+GojoDevloper@users.noreply.github.com>
2024-04-09 10:58:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/GojoDevloper/DAXXMUSIC/tree/Master/DAXXMUSIC",
+    "https://github.com/GojoDevloper/DAXXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
