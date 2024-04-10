f9539bfb1ee9ee67e9ef4776cd4240185eb1aee0
Alishan_Xd <154049785+GojoDevloper@users.noreply.github.com>
2024-04-11 01:58:04 +0530

    Update cheers.py

--- DAXXMUSIC/plugins/Yumi/cheers.py
+++ DAXXMUSIC/plugins/Yumi/cheers.py
@@ -6,9 +6,9 @@ from DAXXMUSIC import app
 @app.on_message(filters.command("cheers"))
 async def es_url(_, message):
     try:
-        api = "https://api.otakugifs.xyz/gif?reaction=kiss&format=gif"
-        get_kiss = get(api)
-        get_json = get_kiss.json()
+        api = "https://api.otakugifs.xyz/gif?reaction=cheers&format=gif"
+        get_cheers = get(api)
+        get_json = get_cheers.json()
         get_gif_url = get_json["url"]
         await message.reply_video(get_gif_url)
     except:
