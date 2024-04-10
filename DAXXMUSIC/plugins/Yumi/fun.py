de98f46a398141234e4c7b353f587c313d8d1fdb
Alishan_Xd <154049785+GojoDevloper@users.noreply.github.com>
2024-04-11 00:01:37 +0530

    Update fun.py

--- DAXXMUSIC/plugins/Yumi/fun.py
+++ DAXXMUSIC/plugins/Yumi/fun.py
@@ -6,16 +6,14 @@ from DAXXMUSIC import app
 
 @app.on_message(filters.command("kiss"))
 async def es_url(_, message):
-    url = "https://api.waifu.pics/sfw/kiss"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        data = response.json()
-        image = data["url"]
-        await message.reply_video(
-            url,
+    ai = "https://api.waifu.pics/sfw/kiss"
+    get_kiss = get(ai)
+    ai_kiss = get_kiss.json()
+    get_gif_url = ai_kiss["url"]
+    await message.reply_video(
+            get_gif_url,
             caption="BY @Anya_forger_probot",  # Add spoiler caption
             parse_mode=ParseMode.MARKDOWN # Enable Markdown parsing
         )
     else:
-        await message.reply_text("Website gyi")
+        await message.reply_text("Website ke mayya chod dala")
