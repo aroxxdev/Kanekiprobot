951c71678537dc351839f5720c369bfa72ebcd88
Alishan_Xd <154049785+GojoDevloper@users.noreply.github.com>
2024-04-11 02:04:21 +0530

    Update kick.py

--- DAXXMUSIC/plugins/Yumi/kick.py
+++ DAXXMUSIC/plugins/Yumi/kick.py
@@ -8,7 +8,7 @@ async def es_url(_, message):
     try:
         api = "https://api.waifu.pics/sfw/kick"
         get_kick = get(api)
-        get_json = get_kocl.json()
+        get_json = get_kick.json()
         get_gif_url = get_json["url"]
         await message.reply_video(get_gif_url)
     except:
