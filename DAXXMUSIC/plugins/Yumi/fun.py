dd795ebda904fc695ea7a399d205030a8b68da29
Alishan_Xd <154049785+GojoDevloper@users.noreply.github.com>
2024-04-10 23:42:53 +0530

    Update fun.py

--- DAXXMUSIC/plugins/Yumi/fun.py
+++ DAXXMUSIC/plugins/Yumi/fun.py
@@ -5,13 +5,13 @@ from pyrogram.types import Message
 from DAXXMUSIC import app
 
 @app.on_message(filters.command("kiss"))
-async def es_gif(_, message):
+async def es_url(_, message):
     url = "https://api.waifu.pics/sfw/kiss"
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
-        image = data["gif"]
+        image = data["url"]
         await message.reply_video(
             gif,
             caption="BY @Anya_forger_probot",  # Add spoiler caption
