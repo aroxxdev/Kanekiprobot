e3d3c6c1a4f34982c69d25be9f732e00cde98d88
aroxxdev <166977752+aroxxdev@users.noreply.github.com>
2024-04-14 12:19:09 +0530

    Update wishcute.py

--- DAXXMUSIC/plugins/Yumi/wishcute.py
+++ DAXXMUSIC/plugins/Yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from DAXXMUSIC import app 
 
-SUPPORT_CHAT = "Anime_Savage_Group"
+SUPPORT_CHAT = "https://t.me/anime_ghoul_group"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
